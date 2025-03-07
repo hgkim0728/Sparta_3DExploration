791ba116c1a77b9ada7729d0480c7aeee15b13c7
hgkim0728 <hgkim0728@gmail.com>
2025-03-07 17:23:49 +0900

    feat_아이템 스크립터블 오브젝트 작성

---
+++ Assets/02.Scripts/Item/ItemData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Consumable,
+    Equip,
+    Resources
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "NewItem")]
+public class ItemData : ScriptableObject
+{
+    [Header("아이템 정보")]
+    [SerializeField, Tooltip("아이템 이름")] private string itemName;
+    public string ItemName { get { return itemName; } }
+    [SerializeField, Tooltip("아이템 설명")] private string itemDescription;
+    public string ItemDescription { get { return itemDescription; } }
+    [SerializeField, Tooltip("아이템 종류")] private ItemType itemType;
+    [SerializeField, Tooltip("아이템의 프리팹")] private GameObject itemPrefab;
+    public GameObject ItemPrefab { get { return itemPrefab; } }
+    public ItemType Type { get { return itemType; } }
+
+    [Header("Consumable")]
+    [SerializeField, Tooltip("아이템 효과 수치")] private int[] itemStats;
+    public int[] ItemStats { get { return itemStats; } }
+    [SerializeField, Tooltip("여러 개를 소지할 수 있는지 여부")] private bool canStack;
+    public bool Stacking { get { return canStack; } }
+    [SerializeField, Tooltip("소지 가능한 최대 개수")] private int maxStack;
+    public int MaxStack { get { return maxStack; } }
+}
